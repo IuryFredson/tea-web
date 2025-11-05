17cea9ebabef01c3a1ac26d6863443af6bf09e64
IuryFredson <iuryfredsongm@gmail.com>
2025-11-05 11:54:04 -0300

    feat(frontend): implementacao react-router e estrutura de paginas

---
+++ frontend/src/pages/ForumPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ForumPage = () => {
+  return (
+    <div className="container mx-auto px-4 py-16">
+      <h1 className="text-4xl font-bold text-center">Fórum de Discussão</h1>
+      <p className="text-xl text-center mt-4">(Em construção...)</p>
+    </div>
+  );
+};
+
+export default ForumPage;
